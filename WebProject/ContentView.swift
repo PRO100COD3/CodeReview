a07c073adcf90e279c715c4c8108bda4fa7475c9
PRO100COD3 <vad.dz2015@yandex.ru>
2024-09-04 14:58:50 +0400

    Initial Commit

---
+++ WebProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebProject
+//
+//  Created by Вадим Дзюба on 04.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebProject/WebProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebProjectApp.swift
+//  WebProject
+//
+//  Created by Вадим Дзюба on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WebProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
